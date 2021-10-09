e72e805d11f6524ef3cb75f952315da294ff5fdf
Rodolfo Baeza <rbaezam@gmail.com>
2021-10-09 16:30:58 -0500

    Changed way of handling files

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -66,17 +66,20 @@ defmodule Servy.Handler do
     file = 
       Path.expand("../../pages", __DIR__)
       |> Path.join("about.html")
+      |> File.read
+      |> handle_file(conv)
+  end
 
-    case File.read(file) do
-      {:ok, content} ->
-        %{ conv | status: 200, resp_body: content }
+  def handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content }
+  end
 
-      {:error, :enoent} ->
-        %{ conv | status: 404, resp_body: "File not found! #{file}" }
+  def handle_file({:error, :enoent}, conv) do
+    %{ conv | status: 404, resp_body: "File not found! #{file}" }
+  end
 
-      {:error, reason} ->
-        %{ conv | status: 500, resp_body: "File error: #{reason}" }
-    end
+  def handle_file({:error, reason}, conv) do
+    %{ conv | status: 500, resp_body: "File error: #{reason}" }
   end
 
   def route(%{path: path} = conv) do
